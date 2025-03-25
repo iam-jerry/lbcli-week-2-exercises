a4f8e980c37c45f6f8bf94083771a5fbb09f355d
Jerry Bature <jerrybature@protonmail.com>
2025-03-25 03:00:56 +0100

    Add bitcoin-cli command

--- submission/03.sh
+++ submission/03.sh
@@ -5,3 +5,4 @@ bitcoin-cli -regtest getnewaddress "change" "bech32"
 bitcoin-cli -regtest generatetoaddress 101 "bcrt1qzgk7yr063wy8ctram5mwcxx52te3qvqff39jh6"
 
 # Return only the Address
+segwit_address=$(bitcoin-cli -regtest getnewaddress "change" "bech32") && echo $segwit_address

cf563403b3d7c14522919eaa5638bf0ec802de2e
Jerry Bature <jerrybature@protonmail.com>
2025-03-25 02:52:14 +0100

    Add bitcoin-cli command

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,7 @@
 # Created a SegWit address.
+bitcoin-cli -regtest getnewaddress "change" "bech32"
+
 # Add funds to the address.
+bitcoin-cli -regtest generatetoaddress 101 "bcrt1qzgk7yr063wy8ctram5mwcxx52te3qvqff39jh6"
+
 # Return only the Address
